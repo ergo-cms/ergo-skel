96ca5f09e776a3b3f6ca2dc0592c7990cc1fca6d
cmroanirgo <cmroanirgo@users.noreply.github.com>
2017-02-04 11:04:20 +1100

    updates information to showcase a little better

--- config.ergo.js
+++ config.ergo.js
@@ -5,13 +5,19 @@ This is a minimal skeleton config for ergo cms. See online for more options
 module.exports = {
 
 	//
-	// These are fields used by the theme
+	// These are fields used within the theme
 	default_fields: {
 		site_url: "http://ergo.example.com/", // ALWAYS end with a '/'
 		site_title: "Your Site", // a short title
 		title: "An ergo-cms skeleton", // this goes in <title>tags generally, but also used in rss 
 		post_image: "/img/post-bg.jpg",
-		author: "Demo Author",
+		image: "page.jpg", // the default image to use.
+		author_image: 'avatar.jpg', // in the /images/ folder
+		//author: "Demo Author", // The default author
+		author_url: '/about.html', // change this to 'authors.html' and edit below 'collate' settings if you have more than one author
+		//tags_url: '/tags.html',
+		//categories_url: '/categories.html',
+		//feed_url: '/rss.xml',
 
 		social: {	// update/remove social entries that you use
 			disqus: 'username',
@@ -25,9 +31,6 @@ module.exports = {
 	},
 
 	theme: "ergo-simple",
-	plugin_options: {
-		// textile: { breaks:false}
-	},
 	default_post_type: "page",
 	post_types: {
 		blog: {
@@ -35,6 +38,7 @@ module.exports = {
 	  		//path: "blog", // ergo defaults to use 'blog'. 
 			default_fields: {
 				//	author: "The Blogger"
+				image: "post.jpg", // the default image for all blog entries
 			},
 		},
 		page: {
@@ -42,5 +46,22 @@ module.exports = {
 		  	//path: "page", // ergo defaults to use 'page', but is ALSO the default_post_type
 		}
 	},
+	plugin_options: {
+		// textile: { breaks:false}
+
+		collate: {
+			post_types: {
+				blog: { as:"blog" },
+			},
+			fields: {
+				// tags and categories are already automatically tracked in the above post_types (initially, 'blog')
+				// Their url is tags.html and categories.html, but onlt if you've used at least ONE!
+
+
+				// uncomment this to track authors 
+				// author: { as: 'authors', base_name:'all' }, // Also, change 'author_url to '/authors.html', above
+			}
+		}
+	}
 	
 };
